10bf6b4173a3716597b60524b51e94160aa46266
Jeni Dang <jenipdang@yahoo.com>
2022-02-02 23:11:02 -0800

    commit tree 9b18f89581a546f373ee93d28bf1838630c3d886

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,16 @@
 class Dog
+    #setter
+    def name=(dogs_name)
+        @this_dogs_name = dogs_name
+    end
 
+    #getter
+    def name
+        @this_dogs_name
+    end
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
+puts lassie.name
